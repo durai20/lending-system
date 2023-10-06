374d2aba5f06fa44cb6adc964d0053e49026cafa
durai20 <duraimurugan.v2003@gmail.com>
2023-10-06 09:24:23 +0530

    updated details

--- php/config.php
+++ php/config.php
@@ -2,6 +2,6 @@
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'database');
+   define('DB_DATABASE', 'datas');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
