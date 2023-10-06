a03f82b8c15e0439aaee8f1ac891574fca31bb16
durai20 <duraimurugan.v2003@gmail.com>
2023-10-06 14:38:28 +0530

    small modification

--- add_customer.php
+++ add_customer.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Add New Students</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-		<script src="bootstrap/js/jquery.min.js"></script>
+
 		<script src="bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="shortcut icon" href="" type="image/x-icon">
